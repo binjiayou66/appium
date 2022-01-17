3d51947f323a3027932dffdc33c6b9fddbcb74f9
Andy <binjiayou66@163.com>
2022-01-17 16:08:20 +0800

    update

--- simples/ios_test.py
+++ simples/ios_test.py
@@ -17,21 +17,19 @@ def stepCordinate(driver: WebDriver, x: int, y: int):
     _stepHoldOn()
 
 
+def stepAccessibilityId(driver: WebDriver, id):
+    element = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value=id)
+    element.click()
+    _stepHoldOn()
+
+
 def stepTextInScreen(driver: WebDriver, text):
     data = driver.get_screenshot_as_png()
     point = cordinate(text, data)
     if not point:
         print('未找到元素')
         return
-    TouchAction(driver=driver).tap(
-        x=point['x'], y=point['y']).release().perform()
-    _stepHoldOn()
-
-
-def stepAccessibilityId(driver: WebDriver, id):
-    element = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value=id)
-    element.click()
-    _stepHoldOn()
+    stepCordinate(driver=driver, x=point['x'], y=point['y'])
 
 
 def stepBack(driver: WebDriver):
@@ -54,18 +52,9 @@ def main():
 
     driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", caps)
 
-    # element = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="任务中心")
-    # element.click()
-
-    # _stepHoldOn()
-    # element = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="历史任务")
-    # element.click()
-
-    # stepTextInScreen(driver=driver, text='任务中心')
-    # stepTextInScreen(driver=driver, text='去完成')
-    # stepTextInScreen(driver=driver, text='审核通过')
-
-    stepAccessibilityId(driver=driver, id="任务中心")
+    stepTextInScreen(driver=driver, text='任务中心')
+    stepTextInScreen(driver=driver, text='去完成')
+    stepTextInScreen(driver=driver, text='审核通过')
     stepBack(driver=driver)
 
     driver.quit()
